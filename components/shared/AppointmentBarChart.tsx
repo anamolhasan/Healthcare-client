56510645fa220a41767b2c42ea425c8e7feb8f7b
anamolhasan <mdanamolhasan1998@gmail.com>
2026-05-09 21:07:30 +0600

    statsCard added

---
+++ components/shared/AppointmentBarChart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppointmentBarChart = () => {
+  return (
+    <div>AppointmentBarChart</div>
+  )
+}
+
+export default AppointmentBarChart
---
+++ components/shared/AppointmentPicChart.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppointmentPicChart = () => {
+  return (
+    <div>AppointmentPicChart</div>
+  )
+}
+
+export default AppointmentPicChart
